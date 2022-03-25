cae3faace55716001f4fe229181187f9064d053c
lalone <sumo229@foxmail.com>
2022-03-25 17:28:14 +0800

    TODO：自动检查

--- tables/vul.go
+++ tables/vul.go
@@ -16,6 +16,14 @@ func GetVulTable(ctx *context.Context) table.Table {
 	info.AddField("ID", "id", db.Int).FieldSortable()
 	info.AddField("名称", "name", db.Varchar)
 	info.AddField("影响范围", "scope", db.Varchar)
+	// TODO
+	// 自动对比，信息插入数据库
+	/*
+		info.AddColumnButtons("自动检查", types.GetColumnButton("启动", icon.Android, action.Ajax("/admin/ajax",
+			func(ctx *context.Context) (success bool, msg string, data interface{}) {
+				return true, "请求成功，奥利给", ""
+			})))
+	*/
 	info.AddField("创建日期", "created_at", db.Timestamp).FieldHide()
 	info.AddField("更新日期", "updated_at", db.Timestamp)
 
